8481f88d372dd9c02e0849a8a068c75f65d77c3e
Mark Booth <mrbooth@gmail.com>
2011-07-29 15:49:58 +0100

    initial commit of blank app - for rollback

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# TV Tracking App #
+
+This is my first attempt at a Rails App
+
+The plan is to have an app that tracks what I have series linked on my Sky+ box
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TvTracker::Application.config.secret_token = '0fc95aebd0e8fa9a8508d26bce7948906aaf34db7189caf0878bf36537678aa680308f19fabd50a7133b61c0576c7b23927842516e3e00c78f029b5027989e6a'
